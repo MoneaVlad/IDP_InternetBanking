c303ef33223360a831680472f0dd5fba435710bf
Vlad Monea <vmonea@fitbit.com>
2020-04-01 00:09:46 +0300

    Add MySql database to stack

--- internet-banking-client-app/internet_banking/__init__.py
+++ internet-banking-client-app/internet_banking/__init__.py
@@ -15,7 +15,8 @@ DB_USERNAME = os.environ["DB_USERNAME"]
 DB_PASS = os.environ["DB_PASS"]
 DB_NAME = os.environ["DB_NAME"]
 DB_HOST = os.environ["DB_HOST"]
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{DB_USERNAME}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}:3306/{}'.format(DB_USERNAME, DB_PASS, DB_HOST, DB_NAME)
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
